0414a1a26a315494c59cda1ef15c776d4c48c24c
saguasantiago <saguasantiago@gmail.com>
2026-08-24 12:49:04 +0000

    first commit

---
+++ preproyecto.h
@@ -0,0 +1,21 @@
+#ifndef CODES_H
+#define INT 0
+#define BOOL 1
+#define VOID 2
+#define MAIN 3
+#define RETURN 4
+#define TRUE 5
+#define FALSE 6
+#define ID 7
+#define NUM 8
+#define SUMA 9
+#define MULTIPLICACION 10
+#define ASIGNACION 11
+#define PARENTESIS_IZQ 12
+#define PARENTESIS_DER 13
+#define LLAVE_IZQ 14
+#define LLAVE_DER 15
+#define PUNTO_COMA 16
+
+
+#endif
